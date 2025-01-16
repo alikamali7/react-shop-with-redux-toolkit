0e6e4c665fcbae2b2bc13a6d5f07f5aa725f5eec
Ali <alikamali0328@gmail.com>
2025-01-16 16:48:08 +0330

    Step 1: Creation of product slice and store

---
+++ src/pages/ProductsPage.jsx
@@ -0,0 +1,59 @@
+import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
+
+import Card from "../components/Card";
+import Loader from "../components/Loader";
+// import { useProducts } from "../context/ProductContext";
+import {
+  searchProducts,
+  filterProducts,
+  getInitialQuery,
+} from "../helpers/helper";
+import styles from "./ProductsPage.module.css";
+import SearchBox from "../components/SearchBox";
+import Sidebar from "../components/Sidebar";
+
+function ProductsPage() {
+  // const products = useProducts();
+  const products = [];
+
+  const [displayed, setDisplayed] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState({});
+
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  useEffect(() => {
+    setDisplayed(products);
+    setQuery(getInitialQuery(searchParams));
+  }, [products]);
+
+  useEffect(() => {
+    setSearchParams(query);
+    setSearch(query.search || "");
+
+    let finalProducts = searchProducts(products, query.search);
+    finalProducts = filterProducts(finalProducts, query.category);
+    setDisplayed(finalProducts);
+  }, [query]);
+
+
+  
+
+  return (
+    <>
+      <SearchBox search={search} setSearch={setSearch} setQuery={setQuery} />
+      <div className={styles.container}>
+        <div className={styles.products}>
+          {!displayed.length && <Loader />}
+          {displayed.map((p) => (
+            <Card key={p.id} data={p} />
+          ))}
+        </div>
+        <Sidebar query={query} setQuery={setQuery} />
+      </div>
+    </>
+  );
+}
+
+export default ProductsPage;
